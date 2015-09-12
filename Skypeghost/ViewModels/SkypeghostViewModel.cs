0df1542f73cae1cdad600c87f0c0366dea67de88
RoryRoberts <rory.the.mover@gmail.com>
2015-09-12 12:56:06 +0100

    Very lazy early UI. Binds to ViewModel properties and non-existant "Copy To Clipboard" command now. Need to actually make that command a thing

--- Skypeghost/ViewModels/SkypeghostViewModel.cs
+++ Skypeghost/ViewModels/SkypeghostViewModel.cs
@@ -3,11 +3,18 @@
     using Microsoft.Practices.Prism.Commands;
     using Microsoft.Practices.Prism.Mvvm;
     using Skypeghost.Models;
+    using System;
     using System.Windows.Input;
 
     public class SkypeghostViewModel : BindableBase
     {
         private ISkypeghostModel model;
+
+        private string userName;
+        private string displayName;
+        private string conversationName;
+        private string quoteText;
+        private DateTime timeStamp = DateTime.Now.ToUniversalTime();
 
         public SkypeghostViewModel(ISkypeghostModel model)
         {
@@ -17,5 +24,29 @@
         }
 
         public ICommand PasteClipboardData { get; private set; }
+
+        public string UserName
+        {
+            get { return this.userName; }
+            set { this.SetProperty(ref this.userName, value); }
+        }
+
+        public string DisplayName
+        {
+            get { return this.displayName; }
+            set { this.SetProperty(ref this.displayName, value); }
+        }
+
+        public string ConversationName
+        {
+            get { return this.conversationName; }
+            set { this.SetProperty(ref this.conversationName, value); }
+        }
+
+        public string QuoteText
+        {
+            get { return this.quoteText; }
+            set { this.SetProperty(ref this.quoteText, value); }
+        }
     }
 }
